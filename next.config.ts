92aa042bd7174bd6dcc35e45664724d09d77bc41
Eason Chai <echai2905@gmail.com>
2025-11-05 12:33:52 +0800

    chore: Update main branch to be complete version of demo

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/siwe",
   reactStrictMode: true,
   output: "standalone",
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
